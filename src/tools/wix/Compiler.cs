a23a9f46045b309029ce5c1caae03b029aeceace
BMurri <osito@live.com>
2015-06-09 00:38:42 -0700

    Improved message and fixed bug

--- src/tools/wix/Compiler.cs
+++ src/tools/wix/Compiler.cs
@@ -11926,17 +11926,17 @@ namespace Microsoft.Tools.WindowsInstallerXml
                                 switch (installPrivilegesType)
                                 {
                                     case Wix.Package.InstallPrivilegesType.elevated:
-                                        if (installScopeSeen && (8 == (sourceBits | 8)))
+                                        if (installScopeSeen && (8 == (sourceBits & 8)))
                                         {
-                                            this.core.OnMessage(WixErrors.IllegalAttributeWithOtherAttribute(sourceLineNumbers, node.Name, attrib.Name, "InstallScope", "perUser"));
+                                            this.core.OnMessage(WixErrors.IncompatiblePackageElevationAttributes(sourceLineNumbers, node.Name, "InstallScope", "perUser", attrib.Name, installPrivileges));
                                         }
                                         // this is the default setting
 
                                         break;
                                     case Wix.Package.InstallPrivilegesType.limited:
-                                        if (installScopeSeen && (0 == (sourceBits | 8)))
+                                        if (installScopeSeen && (0 == (sourceBits & 8)))
                                         {
-                                            this.core.OnMessage(WixErrors.IllegalAttributeWithOtherAttribute(sourceLineNumbers, node.Name, attrib.Name, "InstallScope", "perMachine"));
+                                            this.core.OnMessage(WixErrors.IncompatiblePackageElevationAttributes(sourceLineNumbers, node.Name, "InstallScope", "perMachine", attrib.Name, installPrivileges));
                                         }
                                         sourceBits = sourceBits | 8;
                                         break;
@@ -11955,18 +11955,18 @@ namespace Microsoft.Tools.WindowsInstallerXml
                                 switch (installScopeType)
                                 {
                                     case Wix.Package.InstallScopeType.perMachine:
-                                        if (installPrivilegeSeen && (8 == (sourceBits | 8)))
+                                        if (installPrivilegeSeen && (8 == (sourceBits & 8)))
                                         {
-                                            this.core.OnMessage(WixErrors.IllegalAttributeWithOtherAttribute(sourceLineNumbers, node.Name, attrib.Name, "InstallPrivileges", "limited"));
+                                            this.core.OnMessage(WixErrors.IncompatiblePackageElevationAttributes(sourceLineNumbers, node.Name, attrib.Name, installScope, "InstallPrivileges", "limited"));
                                         }
                                         row = this.core.CreateRow(sourceLineNumbers, "Property");
                                         row[0] = "ALLUSERS";
                                         row[1] = "1";
                                         break;
                                     case Wix.Package.InstallScopeType.perUser:
-                                        if (installPrivilegeSeen && (0 == (sourceBits | 8)))
+                                        if (installPrivilegeSeen && (0 == (sourceBits & 8)))
                                         {
-                                            this.core.OnMessage(WixErrors.IllegalAttributeWithOtherAttribute(sourceLineNumbers, node.Name, attrib.Name, "InstallPrivileges", "elevated"));
+                                            this.core.OnMessage(WixErrors.IncompatiblePackageElevationAttributes(sourceLineNumbers, node.Name, attrib.Name, installScope, "InstallPrivileges", "elevated"));
                                         }
                                         sourceBits = sourceBits | 8;
                                         break;
