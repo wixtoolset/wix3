9fec88c2166989ad00e03d6f4f7d447a609b72d1
Rob Mensching <rob@firegiant.com>
2017-03-01 06:45:15 -0800

    Extract Votive to new VisualStudioExtension repository
    Progress on wixtoolset/issues#5492

--- src/Votive/votive2010/src/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Scope = "type", Target = "Microsoft.Tools.WindowsInstallerXml.VisualStudio.WixPackageSettings+MachineSettingEnum`1")]
-
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Microsoft.Tools.WindowsInstallerXml.Build.Tasks", Justification = "Ignoring this warning...we want this namespace, but don't have enough classes to go in it right now to satisfy the rule.")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Microsoft.Tools.WindowsInstallerXml.VisualStudio.Controls")]
-[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "votive")]
