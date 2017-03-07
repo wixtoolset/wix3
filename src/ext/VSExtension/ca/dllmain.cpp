92dc0e59ea2a8f513fb622489d96a576b255233b
Heath Stewart <heaths@microsoft.com>
2017-03-07 13:46:28 -0800

    Add detection property support for VS2017
    Partly fixes issue #5433 by defining detection properties latest
    installed and highest version of VS2017. The CA code can be easily
    updated to support newer versions.

---
+++ src/ext/VSExtension/ca/dllmain.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+#include "precomp.h"
+
+/********************************************************************
+DllMain - standard entry point for all WiX custom actions
+
+********************************************************************/
+extern "C" BOOL WINAPI DllMain(
+    IN HINSTANCE hInst,
+    IN ULONG ulReason,
+    IN LPVOID)
+{
+    switch(ulReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        WcaGlobalInitialize(hInst);
+        break;
+
+    case DLL_PROCESS_DETACH:
+        WcaGlobalFinalize();
+        break;
+    }
+
+    return TRUE;
+}
