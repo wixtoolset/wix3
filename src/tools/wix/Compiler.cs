970f92c9111efbe194317d6e62e067abbcb1ce77
Nir Bar <nir.bar@panel-sw.co.il>
2017-06-01 16:04:55 +0300

    Support RemotePayload for and Payload (not just ExePackage) Done to reduce bind time of multitude unchanging payloads (e.g. SQL server). Each build hashes them all which consumes a lot of time.

--- src/tools/wix/Compiler.cs
+++ src/tools/wix/Compiler.cs
@@ -20684,6 +20684,9 @@ namespace Microsoft.Tools.WindowsInstallerXml
                     {
                         switch (child.LocalName)
                         {
+                            case "RemotePayload":
+                                break;
+
                             default:
                                 this.core.UnexpectedElement(node, child);
                                 break;
@@ -20766,7 +20769,7 @@ namespace Microsoft.Tools.WindowsInstallerXml
                 {
                     SourceLineNumberCollection childSourceLineNumbers = Preprocessor.GetSourceLineNumbers(child);
 
-                    if (node.NamespaceURI == this.schema.TargetNamespace && node.LocalName != "ExePackage")
+                    if (node.NamespaceURI == this.schema.TargetNamespace && node.LocalName != "ExePackage" && node.LocalName != "Payload")
                     {
                         this.core.OnMessage(WixErrors.RemotePayloadUnsupported(childSourceLineNumbers));
                         continue;
@@ -20867,25 +20870,13 @@ namespace Microsoft.Tools.WindowsInstallerXml
                 }
             }
 
-            if (String.IsNullOrEmpty(remotePayload.Description))
-            {
-                this.core.OnMessage(WixErrors.ExpectedAttribute(sourceLineNumbers, node.Name, "Description"));
-            }
             if (String.IsNullOrEmpty(remotePayload.Hash))
             {
                 this.core.OnMessage(WixErrors.ExpectedAttribute(sourceLineNumbers, node.Name, "Hash"));
             }
-            if (String.IsNullOrEmpty(remotePayload.ProductName))
-            {
-                this.core.OnMessage(WixErrors.ExpectedAttribute(sourceLineNumbers, node.Name, "ProductName"));
-            }
             if (0 == remotePayload.FileSize)
             {
                 this.core.OnMessage(WixErrors.ExpectedAttribute(sourceLineNumbers, node.Name, "Size"));
-            }
-            if (String.IsNullOrEmpty(remotePayload.Version))
-            {
-                this.core.OnMessage(WixErrors.ExpectedAttribute(sourceLineNumbers, node.Name, "Version"));
             }
 
             return remotePayload;
